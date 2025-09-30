4c9ca95f7f6ce164731a9e68690aa9e2c15a387a
Ari <arigato2477@gmail.com>
2025-09-30 18:58:51 +0800

    Update Goodbye.js

--- script/event/Goodbye.js
+++ script/event/Goodbye.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const { createCanvas, loadImage } = require('canvas');
 
 module.exports.config = {
     name: "goodbye",
-    version: "1.2.0",
+    version: "1.3.0",
     eventType: ["log:unsubscribe"],
     description: "Send goodbye card when member leaves"
 };
@@ -16,12 +17,12 @@ module.exports.handleEvent = async function ({ api, event }) {
     let name = await api.getUserInfo(leftID).then(info => info[leftID].name);
 
     const quotes = [
-        "👋 Goodbye, we'll miss you!",
-        "💨 Off you go, take care!",
-        "🌟 Wishing you the best on your journey!",
-        "🚪 Another door closes, good luck ahead!",
-        "📌 You’ll always be remembered here!",
-        "🍀 Farewell, may luck be with you!"
+        "Goodbye, we'll miss you!",
+        "Off you go, take care!",
+        "Wishing you the best on your journey!",
+        "Another door closes, good luck ahead!",
+        "You’ll always be remembered here!",
+        "Farewell, may luck be with you!"
     ];
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
@@ -31,11 +32,24 @@ module.exports.handleEvent = async function ({ api, event }) {
 
     try {
         const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+        const baseImage = await loadImage(Buffer.from(data));
+
+        const canvas = createCanvas(baseImage.width, baseImage.height);
+        const ctx = canvas.getContext('2d');
+        
+        ctx.drawImage(baseImage, 0, 0);
+
+        ctx.font = '18px Arial';
+        ctx.fillStyle = 'rgba(255,255,255,0.7)'; 
+        ctx.textAlign = 'right';
+        ctx.fillText('created by : ARI', baseImage.width - 10, baseImage.height - 10);
+
+        const buffer = canvas.toBuffer();
         const cacheDir = path.join(__dirname, "cache");
         if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir, { recursive: true });
 
         const filePath = path.join(cacheDir, `goodbye_${Date.now()}.png`);
-        fs.writeFileSync(filePath, Buffer.from(data));
+        fs.writeFileSync(filePath, buffer);
 
         api.sendMessage({
             body: `👋 ${name} has left the group.\n💭 Quote: ${randomQuote}`,
